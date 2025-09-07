ad0e9ef31b200583d5cd120c55fdba0f855b1f9d
Caiti Lange <clange441@gmail.com>
2025-09-07 11:14:32 -0500

    block 08 workshop complete

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1;
+code1 = 7 + 3;
+
+let code2 = (100 - 20) / 2;
+
+let code3 = 6 ** 2 + 3;
+
+let message = "The vault has been secured. The combination is: ";
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+console.log(message + codeA);
+console.log(
+  `The vault has been secured. The combination is: ${code1}-${code2}-${code3}`
+);
